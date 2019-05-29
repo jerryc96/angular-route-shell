aa46500f8fa8deb0508636f2bc146983c67ec351
jerryc96 <jerryycheng@gmail.com>
2019-05-28 21:48:12 -0400

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomepageComponent} from './homepage/homepage.component';
+import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'homepage', component: HomepageComponent},
+  {path: '', redirectTo: '/homepage', pathMatch: 'full'},
+  {path: 'login', component: LoginComponent},
+  {path: 'signup', component: SignupComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
